3172663598863cbc807d7761bab35ab837e716da
jianhe <jianhee@qq.com>
2025-02-12 23:43:15 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,30 +1,3 @@
-// 生成随机id
-export function guid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
-
-// 获取设备类型
-export function getDeviceType() {
-  const userAgent = navigator.userAgent.toLowerCase();
-  if (/android/.test(userAgent)) {
-    return 'android';
-  } else if (/iphone|ipad|ipod/.test(userAgent)) {
-    return 'ios';
-  } else {
-    return 'pc';
-  }
-}
-
-// 复制文本
-export function copyText(text) {
-  const input = document.createElement('input');
-  input.value = text;
-  document.body.appendChild(input);
-  input.select();
-  document.execCommand('Copy');
-  document.body.removeChild(input);
-}
+export * from './_fns';
+export * from './_datetime';
+export * from './_toast';
