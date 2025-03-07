7f3ab43c9f12a5aca7c45b5dfdf0032cda266f8f
jianhe <jianhee@qq.com>
2025-03-07 21:41:45 +0800

    update

---
+++ src/components/MxTextRow.vue
@@ -0,0 +1,21 @@
+<!-- 文字行溢出隐藏 -->
+<script>
+import { h } from 'vue';
+
+export default {
+  props: {
+    tag: { type: String, default: 'span' }
+  },
+  setup(props, { slots }) {
+    return () => h(props.tag, { class: 'mx-text-row' }, slots.default && slots.default());
+  }
+};
+</script>
+
+<style lang="scss">
+.mx-text-row {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
