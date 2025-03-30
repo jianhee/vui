960a33cc3fab0d46d4932ca240fde5b45e8037d0
jianhe <jianhee@qq.com>
2025-03-30 21:12:28 +0800

    update

--- src/composables/index.js
+++ src/composables/index.js
@@ -3,7 +3,7 @@ import { defineAsyncComponent } from 'vue';
 /**
  * 自动注册 Vue 全局组件
  * @param {Object} vueApp                 - Vue 应用实例
- * @param {Object} options.vueComponents  - 需要注册的组件集合：import.meta.glob(['./*.vue''])
+ * @param {Object} options.vueComponents  - 需要注册的组件集合：import.meta.glob(['./*.vue'])
  * @param {Array} [options.include=[]]    - 组件名称白名单：空数组时注册全部组件
  * @param {Array} [options.exclude=[]]    - 组件名称黑名单
  */
@@ -12,18 +12,18 @@ export function setupComponents(vueApp, options = {}) {
 
   // 遍历导入的组件并注册
   for (const path in vueComponents) {
-    // 转换组件路径为驼峰命名格式：'./my-component.vue' -> 'MyComponent', './my-component/index.vue' -> 'MyComponent'
-    const componentName = path
-      // 移除开头的./
-      .replace(/^\.\//, '')
-      // 处理index文件
-      .replace(/\/index\.vue$/, '')
-      // 移除扩展名
-      .replace(/\.vue$/, '')
-      .split('-')
-      // 首字母大写
-      .map(part => part[0]?.toUpperCase() + part.slice(1))
-      // 拼接成大驼峰形式
+    // 获取文件名
+    const processedPath = path.replace(/^\.\//, '');
+    const fileName = processedPath
+      .split('/')
+      .pop()
+      .replace(/\.vue$/, '');
+    // 获取组件名：'./my-component.vue' -> 'MyComponent'
+    const componentName = fileName
+      // 同时匹配中划线和下划线
+      .split(/[_-]/)
+      // 转成驼峰式
+      .map(part => part[0]?.toUpperCase() + part.slice(1).toLowerCase())
       .join('');
 
     // 白名单为空 或 组件名在白名单中
@@ -32,6 +32,7 @@ export function setupComponents(vueApp, options = {}) {
     const isExcluded = exclude.includes(componentName);
 
     // 注册组件
+    console.log(componentName);
     if (isIncluded && !isExcluded) {
       vueApp.component(componentName, defineAsyncComponent(vueComponents[path]));
     }
@@ -40,6 +41,6 @@ export function setupComponents(vueApp, options = {}) {
 
 // 注册 MxUi 组件
 export function setupMxComponents(vueApp, options = {}) {
-  const vueComponents = import.meta.glob(['./*.vue', './*/index.vue']);
+  const vueComponents = import.meta.glob(['../components/*.vue', '../components/*/index.vue']);
   setupComponents(vueApp, { vueComponents, ...options });
 }
