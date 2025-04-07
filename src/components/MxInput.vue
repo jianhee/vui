0aad97c1f51be7438ead0dabc52664f4e9625d9a
jianhe <jianhee@qq.com>
2025-04-07 22:48:17 +0800

    update

--- src/components/MxInput.vue
+++ src/components/MxInput.vue
@@ -2,7 +2,7 @@
 <template>
   <div
     class="mx-input"
-    :class="{ 'mx-state-disabled': disabled }"
+    :class="inputClasses"
   >
     <!-- 前置图标 -->
     <MxIconInside
@@ -32,20 +32,31 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import IconClear from '../assets/icons/clear.vue';
 
 const emits = defineEmits(['input', 'change', 'clear', 'enter']);
 
 // 参数
-defineProps({
+const props = defineProps({
   // 占位符
   placeholder: { type: String, default: null },
   // 最大长度
   maxlength: { type: Number, default: null },
   // 禁用状态
   disabled: { type: Boolean, default: false },
+  // 尺寸：small, medium
+  size: { type: String, default: 'medium' },
   // 前置图标：MxIcon 组件的 name/component/props
   prefixIcon: { type: [String, Object], default: null }
+});
+
+// 获取类名
+const inputClasses = computed(() => {
+  return {
+    'mx-state-disabled': props.disabled,
+    [`is-size-${props.size}`]: true
+  };
 });
 
 // 当前值
@@ -75,23 +86,38 @@ function clearValue() {
   gap: 8px;
   align-items: center;
   width: 240px;
-  height: 32px;
   padding: 0 8px;
   border: 1px solid var(--mx-border-color);
   border-radius: 4px;
   &:hover:not(.mx-state-disabled) {
     border-color: var(--mx-brand-color-default);
   }
+
+  // 尺寸
+  &.is-size {
+    &-medium {
+      height: 32px;
+      font-size: 14px;
+    }
+    &-small {
+      height: 24px;
+      font-size: 12px;
+    }
+  }
+
+  // 输入框
   &-inner {
     flex: auto;
     height: 100%;
-    font-size: 14px;
+    font-size: inherit;
     outline: 0;
     background-color: transparent;
   }
   &.mx-state-disabled .mx-input-inner {
     cursor: not-allowed;
   }
+
+  // 图标
   .mx-icon {
     color: var(--mx-input-icon-color);
   }
