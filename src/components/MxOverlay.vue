eaec950785e1cd313923178043d9f9537524415d
jianhe <jianhee@qq.com>
2025-04-18 19:09:29 +0800

    update

---
+++ src/components/MxOverlay.vue
@@ -0,0 +1,17 @@
+<!-- 遮罩层 -->
+<template>
+  <div class="mx-overlay">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+@use '../assets/styles/vars';
+.mx-overlay {
+  position: fixed;
+  inset: 0;
+  z-index: 999;
+  background-color: var(--mx-overlay-bg-color);
+  transition: opacity 0.3s ease;
+}
+</style>
