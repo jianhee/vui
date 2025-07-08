c33f45e6eee9d297061a7d8c875ef3cfa07dbb9e
jianhe <jianhee@qq.com>
2025-07-08 16:13:51 +0800

    update

---
+++ packages/icons/image-error.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M44 23.994a2 2 0 00-4 0h4zm-20-16a2 2 0 100-4v4zm15 32H9v4h30v-4zm-31-1v-30H4v30h4zm32-15v15h4v-15h-4zm-31-16h15v-4H9v4zm0 32a1 1 0 01-1-1H4a5 5 0 005 5v-4zm30 4a5 5 0 005-5h-4a1 1 0 01-1 1v4zm-31-35a1 1 0 011-1v-4a5 5 0 00-5 5h4z"
+      fill="currentColor"
+    />
+    <path
+      d="M6 35l10.693-9.802a2 2 0 012.653-.044L32 36m-4-5l4.773-4.773a2 2 0 012.615-.186L42 31M33 7l8 8m0-8l-8 8"
+      stroke="currentColor"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
